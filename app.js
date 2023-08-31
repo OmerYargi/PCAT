ac61e0f5fa6be826ca92bd73a681fc4826ac9041
Ömer Yargı <yarginomerfaruk@gmail.com>
2023-08-31 10:42:48 +0300

    Static files uploaded

--- app.js
+++ app.js
@@ -1,4 +1,29 @@
-const fn = function () {
-  console.log('funciton working');
-};
-fn();
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Middleware oluşturmak için fonksiyon ve app.use ile çağırma: app.use(express.static('public')); altına app.use(myLogger); yazılarak çağırılır.
+// const myLogger = (req, res, next) => {
+//      console.log('Middleware 1');
+//      next();
+// };
+
+// const myLogger2 = (req, res, next) => {
+//      console.log('Middleware 2');
+//      next();
+// };
+
+// Midlewares
+app.use(express.static('public'));
+// app.use(myLogger);
+// app.use(myLogger2);
+
+app.get('/', (req, res) => {
+     res.sendFile(path.resolve(__dirname, 'temp/index.html'));
+});
+
+const port = 3000;
+app.listen(port, () => {
+     console.log(`Port: ${port} üzerinden yayına başlandı`);
+});
